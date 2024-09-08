34d6e5e75626c8ec1db3b07d2bbade21a333db59
Simon Lund <simon97lund@gmail.com>
2024-09-08 19:19:16 +0200

    :recycle: add raw data dir and auto create dirs if nonexistent

--- config.py
+++ config.py
@@ -1,13 +1,16 @@
-from pathlib import Path
-
 import json
-
+from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent
 
+RAW_DATA_DIR = ROOT_DIR / "raw"
+RAW_DATA_DIR.mkdir(exist_ok=True)
+
 DATA_DIR = ROOT_DIR / "data"
+DATA_DIR.mkdir(exist_ok=True)
 
 HEROES_DIR = DATA_DIR / "heroes"
+HEROES_DIR.mkdir(exist_ok=True)
 
 WIKI_BASE_URL = 'https://deadlocked.wiki'
 
